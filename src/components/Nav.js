121b5db4face460987ac3464f2ca8f998fdb4b69
beckoduje <dujebecko@gmail.com>
2021-06-08 00:06:55 +0300

    08/06/21

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./Logo";
+import NavLinks from "./NavLinks";
+
+export default function Nav() {
+  return (
+    <nav>
+      <Logo />
+      <NavLinks />
+    </nav>
+  );
+}
---
+++ src/components/NavLinks.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function NavLinks() {
+  return (
+    <div className="nav-links-container">
+      <ul className="nav-links-list">
+        <li></li>
+      </ul>
+      <div className="hambi-links">
+        <div className="hambi-cont top">
+          <div className="hambi-long top"></div>
+          <div className="hambi-circle top"></div>
+        </div>
+        <div className="hambi-cont middle">
+          <div className="hambi-long middle"></div>
+          <div className="hambi-circle middle"></div>
+        </div>
+        <div className="hambi-cont bottom">
+          <div className="hambi-long bottom"></div>
+          <div className="hambi-circle bottom"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
