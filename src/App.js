538983b66fcb10464576130b31b46bf1275f0626
beckoduje <dujebecko@gmail.com>
2021-06-23 00:04:30 +0300

    22/06/2021

--- src/App.js
+++ src/App.js
@@ -8,24 +8,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   const [stickyNav, setStickyNav] = useState(false);
 
-  // const [companyData, setCompanyData] = useState();
-  let companyData;
-
-  fetch("https://api.spacexdata.com/v4/company")
-    // fetch vraća Promise
-    // koristimo then metodu da nešto napravimo s promise
-    .then(function (response) {
-      // data moramo vratiti i na njemu koristiti json() metodu da dobijemo data (novi promise)
-      return response.json();
-    })
-    // onda opet then metodu
-    .then(function (data) {
-      companyData = data;
-      console.log(companyData);
-    });
-
-  console.log(companyData);
-
   return (
     <Router>
       <Switch>
--- src/components/about/companyInfo/Leaders.js
+++ src/components/about/companyInfo/Leaders.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import gwynneProfileImg from "../../../images/GwynneShotwell.jpg";
 import muskProfileImg from "../../../images/elon-musk.jpg";
 import mullerProfileImg from "../../../images/tom-muller.jpg";
 
 export default function Leaders() {
+  const [companyData, setCompanyData] = useState();
+
+  fetch("https://api.spacexdata.com/v4/company")
+    // fetch vraća Promise
+    // koristimo then metodu da nešto napravimo s promise
+    .then(function (response) {
+      // data moramo vratiti i na njemu koristiti json() metodu da dobijemo data (novi promise)
+      return response.json();
+    })
+    // onda opet then metodu
+    .then(function (data) {
+      () => {
+        setCompanyData(data);
+      };
+    });
+
+  // useEffect(() => {
+  //   setCompanyData(data);
+  // }, [])
+
+  console.log(companyData);
   return (
     <div className="leaders-container">
       <div className="leader">
