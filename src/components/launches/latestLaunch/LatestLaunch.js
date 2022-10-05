11d98b555b05c072f0d53bf47db5486794c02377
beckoduje <dujebecko@gmail.com>
2022-10-05 13:06:20 +0200

    removed stats from latest launch
    removed stats from latest launch cause API don't provide it.

--- src/components/launches/latestLaunch/LatestLaunch.js
+++ src/components/launches/latestLaunch/LatestLaunch.js
@@ -1,47 +1,7 @@
-import React, { useEffect } from "react";
-import LatestLaunchDescription from "./LatestLaunchDescription";
+import React from "react";
 import LatestLaunchStats from "./LatestLaunchStats";
 
 export default function LatestLaunch() {
-  let slider;
-  let curSlide = 0;
-  let maxSlide;
-
-  useEffect(() => {
-    slider = document.querySelectorAll(".slider-launch");
-    maxSlide = slider.length;
-    init();
-  });
-
-  const goToSlide = function (slide) {
-    slider.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
-    );
-  };
-
-  // Next slide
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    goToSlide(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-    goToSlide(curSlide);
-  };
-
-  const init = function () {
-    goToSlide(0);
-  };
   return (
     <section className="section latest-launch">
       <div className="section-shader">
@@ -51,13 +11,6 @@ export default function LatestLaunch() {
         </div>
         <div className="slides-wrapper">
           <LatestLaunchStats />
-          <LatestLaunchDescription />
-          <button className="slide-btn slide-btn--left" onClick={prevSlide}>
-            <i className="fas fa-arrow-alt-circle-left"></i>
-          </button>
-          <button className="slide-btn slide-btn--right" onClick={nextSlide}>
-            <i className="fas fa-arrow-alt-circle-right"></i>
-          </button>
         </div>
       </div>
     </section>
